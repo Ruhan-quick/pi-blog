09d587ff8497645f3ce54c1bc747b6a860aec1fc
ruhan-quick <ruhanquick@gmail.com>
2021-06-01 11:59:55 +0600

    pagination compelete

--- src/Components/Home/AllArticles/AllArticles.js
+++ src/Components/Home/AllArticles/AllArticles.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
 import Article from "../Article/Article";
 import SideBar from "../SideBar/SideBar";
 import SideBarRight from "../SideBarRight/SideBarRight";
@@ -21,17 +22,45 @@ const AllArticles = () => {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentArticles = allBlogs.slice(indexOfFirstPost, indexOfLastPost);
 
+  //
+  const handlePageNumber = () => {
+    const pp = document.getElementById("postPerPage").value;
+    setPostPerPage(pp);
+    console.log(postsPerPage);
+    paginate(1);
+  };
   //change Page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div>
       <DisplayArticlse allBlogs={currentArticles} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={allBlogs.length}
-        paginate={paginate}
-        style={{ textAlign: "center" }}
-      ></Pagination>
+
+      <div className="flex-style container">
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={allBlogs.length}
+          paginate={paginate}
+          currentPage={currentPage}
+          style={{ textAlign: "center" }}
+        ></Pagination>
+        <Form style={{ marginLeft: "15px" }}>
+          <Form.Group>
+            <Form.Label>Post Per Page</Form.Label>
+            <Form.Control
+              onChange={() => handlePageNumber()}
+              id="postPerPage"
+              as="select"
+            >
+              <option>2</option>
+              <option>3</option>
+              <option>5</option>
+              <option>10</option>
+              <option>20</option>
+              <option>30</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 };
--- src/Components/Home/AllArticles/Pagination/Pagination.js
+++ src/Components/Home/AllArticles/Pagination/Pagination.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Pagination.css";
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
   console.log("pgn", pageNumbers);
+
   return (
     <div>
-      <h3>Hello</h3>
+      <h6>Paginate</h6>
       <ul className="pagination">
+        <li className="page-item">
+          <a
+            onClick={() => {
+              if (currentPage > 1) {
+                paginate(currentPage - 1);
+              }
+            }}
+            href="#"
+            className="page-link"
+          >
+            Previous
+          </a>
+        </li>
         {pageNumbers.map((n) => (
           <li key={n} className="page-item">
             <a onClick={() => paginate(n)} href="#" className="page-link">
@@ -18,6 +32,19 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
             </a>
           </li>
         ))}
+        <li className="page-item">
+          <a
+            onClick={() => {
+              if (currentPage < pageNumbers.length) {
+                paginate(currentPage + 1);
+              }
+            }}
+            href="#"
+            className="page-link"
+          >
+            Next
+          </a>
+        </li>
       </ul>
     </div>
   );
